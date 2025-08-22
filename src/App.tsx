0aecde08d437a3e6384289c69f1775d38eca197b
MdNaimRipto <naimurtsc567@gmail.com>
2025-08-22 16:29:10 +0600

    Base setup completed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+// import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="container px-4 mx-auto">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
